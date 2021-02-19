c48d0f385893d02d708c28bfb1b4f5da004407b1
Zuzanna <zuzannamoskala@gmail.com>
2021-02-19 17:01:49 +0100

    coding challenge 4

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ let continent = 'Europe'
 let population = 'a lot of people';
 
 console.log(country, continent, population);
+
+//Calculate the tip value - coding challenge 4
+
+let tip;
+const bill = [275, 40, 430];
+bill <= 300 && bill >= 50 ? tip = 0.15 : tip = 0.2;
+
+for (const item of bill) {
+    console.log(`The bill was ${item}, the tip was ${item * (bill <= 300 && bill >= 50 ? 0.15 : 0.2)}, and the total value was ${item + (item * (bill <= 300 && bill >= 50 ? 0.15 : 0.2))}`);
+}
